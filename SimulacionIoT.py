ae9241f836bfe3c8fed77e87536a05f5ec2262c1
Alberto Gonzalez <albertoop.gonzalez@gmail.com>
2025-03-06 14:52:35 -0300

    Agregada la infeccion inicial de manera aleatoria y una breve introduccion

--- SimulacionIoT.py
+++ SimulacionIoT.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 class Building:
     def __init__(self, x):
@@ -6,7 +7,6 @@ class Building:
         self.nfloors = [] # Lista de pisos dentro del edificio [Floor]
 
     def configurar_edificio(self, x):        
-        #self.floor = x
         for i in range(self.floor, x):
             self.nfloors.append(Floor(i)) # 0 ---> 7
             y = int(input("Ingrese la cantidad de habitaciones para el piso "+str(i)+": "))
@@ -28,24 +28,49 @@ class Floor:
 class Room:
     def __init__(self, x):
         self.roomid = x
+        self.nzombies = 0
         self.sensor = Sensor()
 
-    def room_state(self):
-        return self.sensor.estado
+    def room_status(self):
+        if self.nzombies == 0:
+            return self.sensor.change_normal()
+        else:
+            return self.sensor.change_alert()
+    
+    def infection(self):
+        self.nzombies += 1
 
 class Sensor:
     def __init__(self):
-        self.estado = "Normal"
+        self.estado = ""
 
-    def cambio_estado_alert(self):
+    def change_alert(self):
         self.estado = "Alert"
+        return self.estado
     
-    def cambio_estado_normal(self):
+    def change_normal(self):
         self.estado = "Normal"
+        return self.estado
 
 class Simulation:
     def __init__(self):
-        pass
+        self.edificio = Building(0)
+        self.infection = False
+
+    def initial_infection(self):
+        self.infection = True
+        floor = random.choice(self.edificio.nfloors)
+        room = random.choice(floor.nrooms)
+        room.infection()
+        return "La infeccion inicial a comenzado en el piso "+ str(floor.floorid)+ " en la habitacion "+str(room.roomid)
+    
+    def building_status(self):
+        for i in range(len(self.edificio.nfloors)): # Rango de la lista de pisos dentro del edificio
+            print("Estado del piso "+str(i))
+            for j in range(len(self.edificio.nfloors[i].nrooms)): # Rango de la lista de habitaciones dentro de cada piso
+                print("Habitacion numero "+str(j) +" "+ str(self.edificio.nfloors[i].nrooms[j].room_status()))
+            pause()
+            clear()
 
 def pause():
     programPause = input("Presione cualquier tecla para continuar...")
@@ -54,7 +79,14 @@ def clear():
     os.system('cls')
 
 def menu_principal():
-    edificio = Building(0)
+    simulation = Simulation()
+    print("Bienvenido a la simulacion de apocalipsis zombie dentro de un edificio")
+    pause()
+    clear()
+    print("Despiertas una mañana y ves a traves de tu ventana que la ciudad es un caos, a iniciado un apocalipsis zombie.")
+    print("Afortunadamente dispones del sistema de sensores dentro del edificio, lo que te dara una vision de como avanza la infeccion dentro de este.")
+    pause()
+    clear()
     while True:
         print("Ingrese una opcion")
         print("1. Configurar edificio")
@@ -70,24 +102,21 @@ def menu_principal():
                 break
             case 1:
                 x = int(input("Ingrese la cantidad de pisos: "))
-                edificio.configurar_edificio(x)
+                simulation.edificio.configurar_edificio(x)
                 clear()
             case 2:
-                for i in range(len(edificio.nfloors)):
-                    print("Estado del piso "+str(i))
-                    for j in range(len(edificio.nfloors[i].nrooms)):
-                        print("Habitacion numero "+str(j) +" "+ str(edificio.nfloors[i].nrooms[j].room_state()))
-                    pause()
-                    clear()
-                pass
+                simulation.building_status()
             case 3:
-                pass
+                if simulation.infection == False:
+                    print(simulation.initial_infection())
+                else:
+                    print("Ya inicio la infeccion que esperas para escapar...")
+                pause()
+                clear()
             case 4:
                 pass
             case 5:
                 pass
             
-
-
 menu_principal()
     
