43ebe85cc005c66f5a177ce15c36e2c7ac289cf7
Alberto Gonzalez <albertoop.gonzalez@gmail.com>
2025-03-06 00:20:49 -0300

    Implementacion de habitaciones para cada piso y visualizacion del estado de cada habitacion

--- SimulacionIoT.py
+++ SimulacionIoT.py
@@ -1,31 +1,37 @@
+import os
+
 class Building:
     def __init__(self, x):
         self.floor = x
         self.nfloors = [] # Lista de pisos dentro del edificio [Floor]
-    
-    def agregar_piso(self, x):
-        floor_x = []
-        self.nfloors.append(floor_x)
 
     def configurar_edificio(self, x):        
+        #self.floor = x
+        for i in range(self.floor, x):
+            self.nfloors.append(Floor(i)) # 0 ---> 7
+            y = int(input("Ingrese la cantidad de habitaciones para el piso "+str(i)+": "))
+            self.nfloors[i].agregar_habitacion(y)        
         self.floor = x
-        for i in range(x):
-            self.nfloors.append(Floor(i))
 
 class Floor:
     def __init__(self, x):
-        self.room = x
+        self.floorid = x
         self.nrooms = [] # Lista de habitaciones dentro de cada piso
 
-    def agregar_habitacion(self, room):
-        self.nrooms.append(room)
+    def agregar_habitacion(self, y):
+        for i in range(len(self.nrooms), y):
+            self.nrooms.append(Room(i))
 
     def visualizar_data(self):
         print(self.room)
 
 class Room:
-    def __init__(self):
-        self.sensor = Sensor
+    def __init__(self, x):
+        self.roomid = x
+        self.sensor = Sensor()
+
+    def room_state(self):
+        return self.sensor.estado
 
 class Sensor:
     def __init__(self):
@@ -41,6 +47,12 @@ class Simulation:
     def __init__(self):
         pass
 
+def pause():
+    programPause = input("Presione cualquier tecla para continuar...")
+
+def clear():
+    os.system('cls')
+
 def menu_principal():
     edificio = Building(0)
     while True:
@@ -51,20 +63,22 @@ def menu_principal():
         print("4. Limpiar habitacion")
         print("5. Bloquear habitacion")
         print("0. Salir")
-        n = input()
-        match(int(n)):
+        n = int(input())
+        clear()
+        match(n):
             case 0:
                 break
             case 1:
-                x = input("Ingrese la cantidad de pisos: ")
-                edificio.configurar_edificio(int(x))
-                #for i in range(int(x)):
-                    #y = input("Ingrese la cantidad de habitaciones para el piso "+str(i)+": ")
-                    #edificio.agregar_piso(int(y))
-
-                pass
+                x = int(input("Ingrese la cantidad de pisos: "))
+                edificio.configurar_edificio(x)
+                clear()
             case 2:
-                #print(edificio.nfloors[4])
+                for i in range(len(edificio.nfloors)):
+                    print("Estado del piso "+str(i))
+                    for j in range(len(edificio.nfloors[i].nrooms)):
+                        print("Habitacion numero "+str(j) +" "+ str(edificio.nfloors[i].nrooms[j].room_state()))
+                    pause()
+                    clear()
                 pass
             case 3:
                 pass
